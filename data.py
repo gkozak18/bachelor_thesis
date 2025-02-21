2f12f8983fbad85563e02cf2333305aa3c2094d4
Hryhorii Kozak <h.kozak@devrain.com>
2025-02-21 12:32:35 +0100

    first training, absolut test loss is already 8.11 with MAE Loss

--- data.py
+++ data.py
@@ -1,53 +1,48 @@
 import pandas as pd
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import torch
 from tqdm.notebook import tqdm
 import cv2
+import os
 
 
-class Dataset:
-    def __init__(self, folder : str, save_in_ram : bool =True):
+class Dataset(Dataset):
+    def __init__(self, folder : str, size : tuple = (320, 320), save_in_ram : bool =True, device : str = "cuda"):
         """
         folder: str
             Path to the folder containing the data
+        size: tuple
+            Size of the images
         save_in_ram: bool
             If True, the images will be loaded in memory
+        device: str
+            Device to use
         """
         self.folder = folder
-        self.train_annotation = pd.read_csv(f"{folder}/train/annotation.csv")
-        self.test_annotation = pd.read_csv(f"{folder}/test/annotation.csv")
+        self.device = device
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(size)
+        ])
+        self.annotation = pd.read_csv(f"{folder}/annotation.csv")
         self.save_in_ram = save_in_ram
-        self.train_patients = self.train_annotation["patient"].unique()
-        self.test_patients = self.test_annotation["patient"].unique()
+        self.patients = self.annotation["patient"].unique()
 
-        self.train_x = {
-            patient: [] for patient in self.train_patients
-        }
-        self.train_y = {
-            patient: [] for patient in self.train_patients
-        }
-        print("Loading train data")
-        for i, row in tqdm(self.train_annotation.iterrows(), total=len(self.train_patients)):
+        self.x = []
+        self.y = []
+        print(f"Loading data from {folder}")
+        for i, row in tqdm(self.annotation.iterrows(), total=len(self.annotation)):
             if save_in_ram:
-                img = cv2.imread(f"{folder}/{row['path']}")
+                img = cv2.imread(f"{folder}/{os.path.split(row['path'])[-1]}")
             else:
                 img = f"{folder}/{row['path']}"
-            self.train_x[row["patient"]].append(img)
-            self.train_y[row["patient"]].append(row["target"])
-
-        self.test_x = {
-            patient: [] for patient in self.test_patients
-        }
-        self.test_y = {
-            patient: [] for patient in self.test_patients
-        }
-        print("Loading test data")
-        for i, row in tqdm(self.test_annotation.iterrows(), total=len(self.test_patients)):
-            if save_in_ram:
-                img = cv2.imread(f"{folder}/{row['path']}")
-            else:
-                img = f"{folder}/{row['path']}"
-            self.test_x[row["patient"]].append(img)
-            self.test_y[row["patient"]].append(row["target"])
+            self.x.append(img)
+            self.y.append(row["target"])
         print("Data loaded")
-        print(f"Train patients: {len(self.train_patients)}")
-        print(f"Test patients: {len(self.test_patients)}")
-        
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, idx):
+        x = self.transform(self.x[idx]).to(self.device)
+        y = torch.tensor([self.y[idx]]).to(self.device).float()
+        return x, y
